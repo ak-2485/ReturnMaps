fix derivative, loop on data gen, still commented for samp test

HenonNets/resonant_magnetic/poinplot_res_mag.py
@@ -15,16 +15,16 @@ def get_eps():
 
 def zdot(z,phi,e):
     x      = z[:,0:1]
-    y      = z[:,1:]
+    y      = z[:,1:2]
     arg_a  = (x-y) * x**2 * np.cos(3*phi)
     arg_b  = (x-y) * x**2 * np.sin(3*phi)
-    xdot_a = (1 - np.tanh(arg_a)**2) * (3*x**2 - 2*x*y)*np.cos(3*phi) 
-    xdot_b = (1 - np.tanh(arg_b)**2) * (3*x**2 - 2*x*y)*np.sin(3*phi)
+    xdot_a = (1 - (np.tanh(arg_a))**2) * (3*x**2 - 2*x*y)*np.cos(3*phi) 
+    xdot_b = (1 - (np.tanh(arg_b))**2) * (3*x**2 - 2*x*y)*np.sin(3*phi)
+    ydot_a = (1 - (np.tanh(arg_a))**2) * -(x**2 * np.cos(3*phi))
+    ydot_b = (1 - (np.tanh(arg_b))**2) * -(x**2 * np.sin(3*phi))
     xdot   = 0.5*x + 0.25*e*(xdot_a + 0.2*xdot_b) 
-    ydot_a = (1 - np.tanh(arg_a)**2) * -(x**2 * np.cos(3*phi))
-    ydot_b = (1 - np.tanh(arg_b)**2) * -(x**2 * np.sin(3*phi))
     ydot   = y + 0.25*e*(ydot_a + 0.2*ydot_b)
-    return np.hstack([xdot,ydot])
+    return np.hstack([ydot,-xdot])
 
 def rk_pmap(z,eps,n_rk_steps = 100):
     dphi = 2*np.pi/n_rk_steps
@@ -41,7 +41,7 @@ def rk_pmap(z,eps,n_rk_steps = 100):
 
 def gen_samples_ellipse(origin,r_major,r_minor,n_samples):
     """
-    Generate random points inside a circlue of radius 1, ensuring
+    Generate random points inside a circle of radius 1, ensuring
     uniformity in ellipse by taking sqrt of resulting radius.
     """
     r = np.random.uniform(0.0,1.0,n_samples)
@@ -53,16 +53,19 @@ def gen_samples_ellipse(origin,r_major,r_minor,n_samples):
     return np.hstack([x.reshape(n_samples,1),y.reshape(n_samples,1)])
 
 def gen_samples_pmap(origin,r1,r2,nics,n_iterations,eps):
-    rkstep=2000
+    rkstep   = 2000
+    interval = 10
     latent_samples = gen_samples_ellipse(origin, r1,r2,nics)
-    sample = latent_samples
-    n_samples=(n_iterations//10)*nics
+    n_samples=(n_iterations//interval)*nics
     samples  = np.zeros([n_samples,2])
-    samples[0:nics,:] = latent_samples
+    samples[0:nics,:] = latent_samples[:,:]
+    sample = latent_samples[:,:]
     for i in range(n_iterations):
         sample = rk_pmap(sample,eps,rkstep)
-        if (i+1 % 10 == 0 ):
-            samples[(i+1)*nics:(i+2)*nics,:] = sample[:,:]
+        print(sample)
+        if (i % interval == 0 and i != 0):
+            ind = i//interval
+            samples[ind*nics:(ind+1)*nics,:] = sample[:,:]
     return samples
 
 def gen_labels(samples, nics, n_iterations, eps):
@@ -78,7 +81,8 @@ def gen_labels(samples, nics, n_iterations, eps):
 r1=1.75
 r2=1.0
 eps=get_eps()
-data_raw = gen_samples_pmap([0,0], r1,r2, 10000, 400,eps)
+data_raw = gen_samples_pmap([0,0], r1,r2, 100, 40,eps)
+sys.exit()
 #labels_raw = gen_labels(data_raw, 400000, 1,eps)
 print("done generating labels and data")
 print(np.shape(data_raw))
@@ -103,7 +107,7 @@ fig, ax = plt.subplots()
 plt.plot(data[:,0],data[:,1],'r.',markersize=1)
 ax.set_title('Poincare map input')
 plt.savefig('res_mag_in_data.png')
-
+print("finished plotting")
 sys.exit()
 rate_init = 0.1
 def scheduler(epoch):