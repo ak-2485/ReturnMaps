7c2a5abe70fc972492f99da71100b5982f51ffed
ak2485 <ak2485@cornell.edu>
2021-05-19 20:36:38 -0400

    fix integrators

--- HenonNets/Integrators.py
+++ HenonNets/Integrators.py
@@ -7,7 +7,7 @@ import sys
 sys.path.append('../')
 import pickle
 
-def rk_pmap(z,eps,n_rk_steps = 100):
+def rk4(z,eps,n_rk_steps = 100):
     dphi = 2*np.pi/n_rk_steps
     phi_current = 0.0
     z_current = 1.0*z
@@ -20,43 +20,48 @@ def rk_pmap(z,eps,n_rk_steps = 100):
         phi_current = phi_current + dphi
     return z_current
 
-def leapfrog(z,eps,n_lf_steps):
+def leapfrog2(z,DU,eps,n_lf_steps):
+    z_current = 1.0*z
+    q_new     = 1.0*z[:,0]
     dphi = 2*np.pi/n_lf_steps
     phi_current = 0.0
-    z0   = 1.0*z
-    # midpoint for update
-    z_arg1    = 1.0*z0
-    z_arg2    = 1.0*z0
-    z_current = 1.0*z0
     for i in range(n_lf_steps):
-        z_up        = zdot(z_current,phi_current,eps)
-        z_new       = z_current + dphi*zdot(z_current + .5*dphi*z_up, phi_current + .5*dphi,eps)
-        y_mid     = (z_current[:,1] + z_new[:,1])/2.0
-        z_arg1[:,0] = z_current[:,0]
-        z_arg1[:,1] = y_mid
-        z_arg2[:,0] = z_new[:,0]
-        z_arg2[:,1] = y_mid
-        x_new      = z_current[:,0] + 0.5*dphi*( (zdot(z_arg1,phi_current , 0.25))[:,0] + (zdot(z_arg2,phi_current , 0.25))[:,0])
-        y_mid      = z_current[:,1] - 0.5*dphi* (zdot(z_arg1,phi_current , 0.25))[:,1]
-        z_arg2[:,0] = x_new
-        z_arg2[:,1] = y_mid
-        y_new      = z_current[:,1] - 0.5*dphi* (zdot(z_arg2, phi_current , 0.25))[:,1]
-        z_current[:,0]  = x_new
-        z_current[:,1]  = y_new
+        q_new = z_current[:,0] + dphi * z_current[:,1] - 0.5*dphi**2 * DU(z_current[:,0])
+        z_current[:,1] = z_current[:,1] - 0.5*dphi*(DU(z_current[:,0]) + DU(q_new))
+        z_current[:,0] = q_new
+        phi_current = phi_current + dphi
     return z_current
 
-#H = p^2/2 - cos(q)
-"""
-def zdot(z,t,eps):
-    X = z[:,0:1]
-    Y = z[:,1:2]
-    return np.hstack([-np.sin(Y),X])
 
-x       = np.random.uniform(0.0,1.0,1000)
-y       = np.random.uniform(-1.0,1.0,1000)
-samples = np.hstack([x.reshape(1000,1),y.reshape(1000,1)])
-rk4     = rk_pmap(samples ,0.25,2000)
-lf      = leapfrog(samples,0.25,2000)
-plt.scatter(rk4[:,0],rk4[:,1])
+"""
+# H(q,p) = p^2/2 - cos(q)
+def zdot(z,t,eps):
+    Q = z[:,0:1]
+    P = z[:,1:2]
+    return np.hstack([-P,np.sin(Q)])
+
+ydata=np.array([[0.5, 1.0, 1.5]])
+xic = np.zeros([3,1])
+yic = ydata.transpose()
+zic = np.hstack([xic,yic])
+
+rk_state_model=1.0*zic
+rk_model = np.zeros([3,2,1000+1])
+lf_state_model=1.0*zic
+lf_model = np.zeros([3,2,1000+1])
+
+for i in range(1000+1):
+    lf_model[:,:, i] = lf_state_model
+    rk_model[:,:, i] = rk_state_model
+    rk_state_model = rk4(rk_state_model,.1,20)
+    lf_state_model = leapfrog2(lf_state_model,np.sin,.1,20)
+
+xplot1 = np.ravel(rk_model[:,0,:])
+yplot1 = np.ravel(rk_model[:,1,:])
+xplot2 = np.ravel(lf_model[:,0,:])
+yplot2 = np.ravel(lf_model[:,1,:])
+
+plt.scatter(xplot2,yplot2)
+#plt.scatter(lf[:,0],lf[:,1])
 plt.show()
 """
